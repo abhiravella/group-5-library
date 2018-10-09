46eae570fa480dac1aab399898362b17fa45f50c
mrskywilliams <43656648+mrskywilliams@users.noreply.github.com>
2018-10-09 16:32:04 -0400

    Added a ListofBooks class Added properties to Books class

--- Library/Books.cs
+++ Library/Books.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Books
+    class Books : ListofBooks
     {
+        public string Title
+        {
+            get; set;
+        }
+        public string Author
+        {
+            get; set;
+        }
+        public string Genre
+        {
+        get; set;
+        }
+        public int Pubyear
+        {
+            get; set;
+        }
+
+        public bool available = true;
+        public Books(string title, string author, string category, int publishyear)
+        {
+            Title = title;
+            Author = author;
+            Genre = category;
+            Pubyear = publishyear;
+        }
     }
 }
---
+++ Library/ListofBooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    //A list to store the books
+    class ListofBooks
+    {
+        private List<ListofBooks> books = new List<ListofBooks>();
+        public void AddBook(string booktitle, string bookauthor, string bookcategory, int bookpublishyear)
+        {
+            books.Add(new Books(booktitle, bookauthor, bookcategory, bookpublishyear));
+        }
+        public string BookName { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+    
