8b17c31b77c8fa356adde870d6795ad32b8f9fea
Abhimanyu Ravilla <abhiravilla@gmail.com>
2018-10-09 18:57:29 -0400

    added comments on Books class

--- Library/Books.cs
+++ Library/Books.cs
@@ -15,14 +15,23 @@ namespace Library
         private List<int> available = new List<int>();
         #endregion
         #region methods
+        /*
+         *  Returns number of Books in the Library
+         */
         public static int getCount()
         {
             return bookCount;
         }
+        /*
+         *  Updates number of books when adding a new book
+         */
         private static void updateCount()
         {
             bookCount++;
         }
+        /*
+         *  Adds a new book to the library. If one of the field is null pass it as empty string or null
+         */
         public void AddBook(string bookName, string bookAuthor, string bookGenere, int bookYear,int bookavailable)
         {
             name.Add(bookName.ToUpper());
@@ -35,6 +44,7 @@ namespace Library
             return;
         }
         /*
+         *  Searches the library based on the value given and field user wants. The values for field are given below
          *  Field value 
          *  1 = Name
          *  2 = Author
@@ -48,27 +58,27 @@ namespace Library
             switch (field)
             {
                 case 1:
-                    indexes = SearchResultString(searchValue.ToUpper(), name);
+                    indexes = SearchResult(searchValue.ToUpper(), name);
                     Display(indexes);
                     break;
                 case 2:
-                    indexes = SearchResultString(searchValue.ToUpper(), author);
+                    indexes = SearchResult(searchValue.ToUpper(), author);
                     Display(indexes);
                     break;
                 case 3:
-                    indexes = SearchResultString(searchValue.ToUpper(), genere);
+                    indexes = SearchResult(searchValue.ToUpper(), genere);
                     Display(indexes);
                     break;
                 case 4:
                     if (String.IsNullOrEmpty(searchValue) || String.IsNullOrWhiteSpace(searchValue))
                         return;
-                    indexes = SearchResultInt(Convert.ToInt32(searchValue), year);
+                    indexes = SearchResult(Convert.ToInt32(searchValue), year);
                     Display(indexes);
                     break;
                 case 5:
                     if (String.IsNullOrEmpty(searchValue) || String.IsNullOrWhiteSpace(searchValue))
                         return;
-                    indexes = SearchResultInt(Convert.ToInt32(searchValue), year);
+                    indexes = SearchResult(Convert.ToInt32(searchValue), year);
                     Display(indexes);
                     break;
                 default:
@@ -76,7 +86,10 @@ namespace Library
             }
             return;
         }
-        private List<int> SearchResultString(string value, List<String> searchList)
+        /*
+         * SearchResult --- Overloaded method. It will search based on userinputs. If year or availability are search parameters then method with Integer parameters is used.
+         */
+        private List<int> SearchResult(string value, List<String> searchList)
         {
             List<int> resultIndex = new List<int>();
             for (int i = 0;i < searchList.Count; i++)           
@@ -88,7 +101,7 @@ namespace Library
             }
             return resultIndex;
         }
-        private List<int> SearchResultInt(int value, List<int> searchList)
+        private List<int> SearchResult(int value, List<int> searchList)
         {        
             List<int> resultIndex = new List<int>();
             for (int i = 0; i < searchList.Count; i++)
@@ -100,6 +113,9 @@ namespace Library
             }
             return resultIndex;
         }
+        /*
+         *  Displays all the details of the book based on return value of SearchResult
+         */
         private void Display(List<int> indexes)
         {
             if(indexes.Count == 0)
