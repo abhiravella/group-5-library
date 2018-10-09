96a7901d4de62ffb47530e24192296c6b23dccfe
swapna komala <gannamaneni.swapna@gmail.com>
2018-10-09 19:13:53 -0400

    Crearted  Person, Admin, Patron classes

--- Library/Person.cs
+++ Library/Person.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Library
 {
-    class Person
+    public class Person
     {
+        private String name;
+        private int id;
+        private int contactNo;
+
     }
+
+    public class Student : Person
+    {
+
+
+    }
+
+
+    public class Admin : Person
+    {
+        private List<String> bookname = new List<String>();
+        private List<String> studentname = new List<String>();
+        private List<int> studentid = new List<int>();
+        private List<DateTime> issuedate = new List<DateTime>();
+
+        public void LendBooks(string bookName, string studentName, int studentId, DateTime date)
+        {
+            bookname.Add(bookName.ToUpper());
+            studentname.Add(studentName.ToUpper());
+            studentid.Add(studentId);
+            issuedate.Add(date);
+
+            return;
+        }
+
+        public void AuditBooks()
+        {
+
+        }
+
+    }
+
 }
